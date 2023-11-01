7dcd8f7676c522a396134333e67b4d1cb7c9749b
jKurdziel <qwercy142@gmail.com>
2023-11-01 18:34:49 +0100

    add icons

---
+++ src/views/InsertView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Flashcards from '@/components/Flashcards.vue'
+const flashcards = ref<string[][]>([
+  ['Pies', 'Dog'],
+  ['Kot', 'Cat']
+])
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Insert Here your words</h1>
+
+    <Flashcards :flashcards="flashcards" />
+
+    <RouterLink to="/saved">Save</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+}
+</style>
